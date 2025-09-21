0b00a8a5b7b9b8a95ad7a0cff86ee6b7003a82e3
abeldaverio <abel.daverio@epitech.eu>
2025-09-21 17:26:01 +0200

    feat: run tests workflows

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+module Main (main) where
+
+import Test.HUnit
+import qualified System.Exit as Exit
+
+testHello :: Test
+testHello = TestCase $ assertEqual "should success" "hello" "hello"
+
+tests :: Test
+tests = TestList [TestLabel "hello" testHello]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    result <- runTestTT tests
+    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
