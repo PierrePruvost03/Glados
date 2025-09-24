78d10bb91b49bb566ba94b2bc11ad584d1eb3a7f
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-09-24 14:18:05 +0200

    feat(interpreter): basic interpreter for AValue, ASymbol and AList

--- src/Interpreter/EvalAst.hs
+++ src/Interpreter/EvalAst.hs
@@ -1,12 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Interpreter.EvalAst
   ( astValueToValue
+  , evalAst
   ) where
 
-import DataStruct.Ast (AstValue(..))
-import Interpreter.BaseEnv (Value(..))
+import DataStruct.Ast (AstValue(..), Ast(..))
+import Interpreter.BaseEnv (Value(..), Env, lookupEnv, err)
 import Parser (LineCount)
 
 astValueToValue :: (LineCount, AstValue) -> Value
 astValueToValue (_, AstInteger i) = VInt i
 astValueToValue (_, AstBool b) = VBool b
 astValueToValue (_, AstString s) = VString s
+
+evalAst :: Env -> Ast -> Either String Value
+evalAst env = \case
+  AValue val -> Right $ astValueToValue val
+  ASymbol (lc, s) -> maybe (err lc $ "Unbound variable: " ++ s) Right (lookupEnv s env)
+  AList (_, xs) -> VList <$> traverse (evalAst env) xs
+  _ -> Left "Not implemented yet"
