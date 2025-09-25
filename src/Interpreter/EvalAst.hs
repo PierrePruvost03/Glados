4bbf40e7124e80303563b8e74253b75d48a5b24f
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-09-25 11:07:17 +0200

    feat(interpreter): evalAst had to return env cause it can modify it (when user define somthing). Adapted tests with this change too.

--- src/Interpreter/EvalAst.hs
+++ src/Interpreter/EvalAst.hs
@@ -9,28 +9,68 @@ module Interpreter.EvalAst
 import DataStruct.Ast (AstValue(..), Ast(..), AstLambda(..))
 import Interpreter.BaseEnv (Value(..), Env, lookupEnv, extendEnv)
 import Parser (LineCount)
+import Control.Monad (foldM)
 
 astValueToValue :: (LineCount, AstValue) -> Value
 astValueToValue (_, AstInteger i) = VInt i
 astValueToValue (_, AstBool b) = VBool b
 astValueToValue (_, AstString s) = VString s
 
-evalAst :: Env -> Ast -> Either String Value
-evalAst _ (AValue v) = Right $ astValueToValue v
+evalAst :: Env -> Ast -> Either String (Value, Env)
+evalAst env (AValue v) = Right (astValueToValue v, env)
 evalAst env (ASymbol (_, s)) =
     case lookupEnv s env of
-      Just val -> Right val
+      Just val -> Right (val, env)
       Nothing -> Left $ "Unbound variable: " ++ s
-evalAst env (AList (_, xs)) = VList <$> traverse (evalAst env) xs
+evalAst env (AList (_, xs)) = 
+  foldM evalListItem ([], env) xs >>= \(vals, newEnv) -> 
+    Right (VList (reverse vals), newEnv)
+  where
+    evalListItem (acc, currentEnv) ast = 
+      evalAst currentEnv ast >>= \(val, updatedEnv) ->
+        Right (val : acc, updatedEnv)
 evalAst env (ALambdas (_, AstLambda params body)) =
-  VLambda <$> traverse getParam params <*> pure body <*> pure env
+  case traverse getParam params of
+    Right paramNames -> Right (VLambda paramNames body env, env)
+    Left err -> Left err
   where
     getParam (ASymbol (_, s)) = Right s
     getParam x = Left $ "Invalid parameter: " ++ show x
 evalAst env (AIf (_, cond) (_, t) (_, e)) =
-  evalAst env cond >>= \v ->
-    case v of
+  evalAst env cond >>= \(condVal, _) ->
+    case condVal of
       VBool True -> evalAst env t
       VBool False -> evalAst env e
       _ -> Left "Condition is not a boolean"
-evalAst _ _ = Left "Not implemented yet"
+evalAst env (ADefine (_, varName) (_, v)) =
+  evalAst env v >>= \(val, _) ->
+    Right (val, extendEnv env [(varName, val)])
+evalAst env (ACall (_, funcName) (_, arg)) =
+  case lookupEnv funcName env of
+    Nothing -> Left $ "Unbound function: " ++ funcName
+    Just func -> callFunction funcName env func arg
+
+callFunction :: String -> Env -> Value -> Ast -> Either String (Value, Env)
+callFunction _ callEnv (VLambda params body lambdaEnv) argAst =
+  evaluateArgs callEnv argAst >>= \(argValues, _) ->
+    case length params == length argValues of
+      False -> Left $ "Wrong number of arguments: expected " ++ show (length params) 
+                      ++ ", got " ++ show (length argValues)
+      True -> evalAst (extendEnv lambdaEnv (zip params argValues)) body >>= \(result, _) ->
+        Right (result, callEnv)
+callFunction _ callEnv (VPrim _ primFunc) argAst =
+  evaluateArgs callEnv argAst >>= \(argValues, newEnv) ->
+    primFunc argValues >>= \result -> Right (result, newEnv)
+callFunction funcName _ _ _ = Left $ funcName ++ " is not a function"
+
+evaluateArgs :: Env -> Ast -> Either String ([Value], Env)
+evaluateArgs argEnv (AList (_, argList)) = 
+  foldM evalArgItem ([], argEnv) argList >>= \(vals, finalEnv) ->
+    Right (reverse vals, finalEnv)
+  where
+    evalArgItem (acc, currentEnv) ast = 
+      evalAst currentEnv ast >>= \(val, updatedEnv) ->
+        Right (val : acc, updatedEnv)
+evaluateArgs argEnv singleArg = 
+  evalAst argEnv singleArg >>= \(val, newEnv) ->
+    Right ([val], newEnv)
--- test/EvalAstTests.hs
+++ test/EvalAstTests.hs
@@ -1,20 +1,20 @@
 module EvalAstTests (evalAstTests) where
 
 import Test.HUnit
-import Interpreter.EvalAst (evalAst, astValueToValue)
+import Interpreter.EvalAst (evalAst)
 import Interpreter.BaseEnv (Value(..), emptyEnv, extendEnv)
 import DataStruct.Ast (Ast(..), AstValue(..))
 
 testEvalAstValue :: Test
 testEvalAstValue =
   TestCase (assertEqual "should evaluate AstValue to Value"
-    (Right (VInt 42))
+    (Right (VInt 42, emptyEnv))
     (evalAst emptyEnv (AValue ((0,0), AstInteger 42))))
 
 testEvalAstSymbolFound :: Test
 testEvalAstSymbolFound =
   TestCase (assertEqual "should lookup symbol in environment"
-    (Right (VBool True))
+    (Right (VBool True, extendEnv emptyEnv [("x", VBool True)]))
     (evalAst (extendEnv emptyEnv [("x", VBool True)]) (ASymbol ((0,0), "x"))))
 
 testEvalAstSymbolNotFound :: Test
@@ -27,7 +27,7 @@ testEvalAstSymbolNotFound =
 testEvalAstIfCondition :: Test
 testEvalAstIfCondition =
   TestCase (assertEqual "should evaluate if condition correctly"
-    (Right (VInt 10))
+    (Right (VInt 10, emptyEnv))
     (evalAst emptyEnv (AIf ((0,0), AValue ((0,0), AstBool True)) 
                           ((0,0), AValue ((0,0), AstInteger 10))
                           ((0,0), AValue ((0,0), AstInteger 20)))))
@@ -35,12 +35,18 @@ testEvalAstIfCondition =
 testEvalAstList :: Test
 testEvalAstList =
   TestCase (assertEqual "should evaluate list of values"
-    (Right (VList [VInt 1, VBool True, VString "hello"]))
+    (Right (VList [VInt 1, VBool True, VString "hello"], emptyEnv))
     (evalAst emptyEnv (AList ((0,0), [
         AValue ((0,0), AstInteger 1),
         AValue ((0,0), AstBool True),
         AValue ((0,0), AstString "hello")
       ]))))
+
+testEvalAstDefine :: Test
+testEvalAstDefine =
+  TestCase (assertEqual "should define variable and update environment"
+    (Right (VInt 42, extendEnv emptyEnv [("x", VInt 42)]))
+    (evalAst emptyEnv (ADefine ((0,0), "x") ((0,0), AValue ((0,0), AstInteger 42)))))
 
 evalAstTests :: [Test]
 evalAstTests = [
@@ -48,5 +54,6 @@ evalAstTests = [
         TestLabel "evalAst ASymbol found" testEvalAstSymbolFound,
         TestLabel "evalAst ASymbol not found" testEvalAstSymbolNotFound,
         TestLabel "evalAst AIf condition" testEvalAstIfCondition,
-        TestLabel "evalAst AList" testEvalAstList
+        TestLabel "evalAst AList" testEvalAstList,
+        TestLabel "evalAst ADefine" testEvalAstDefine
     ]
