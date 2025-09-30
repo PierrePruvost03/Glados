44407a2213966130642538274f7223abf0197beb
Polo <paul.berlioz@epitech.eu>
2025-09-30 12:25:24 +0200

    feat(main): adding a way to read code from terminal as GHCI until all parenthesis are closed

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,38 @@
 module Main (main) where
 
-import Lib
+-- import Lib
+
+isOpenPar :: Char -> Int
+isOpenPar '(' = 1
+isOpenPar _ = 0
+
+isClosedPar :: Char -> Int
+isClosedPar ')' = 1
+isClosedPar _ = 0
+
+countOpenPar :: String -> Int
+countOpenPar txt = sum (map isOpenPar txt)
+
+countClosedPar :: String -> Int
+countClosedPar txt = sum (map isClosedPar txt)
+
+compareEnoughPar :: String -> Bool
+compareEnoughPar txt = countOpenPar txt <= countClosedPar txt
+
+getUserInput :: Maybe String -> IO ()
+getUserInput Nothing = do
+    content <- getLine
+    let result = content
+    if null content && compareEnoughPar result
+        then putStrLn result
+        else getUserInput $ Just result
+getUserInput (Just txt) = do
+    content <- getLine
+    let result = txt ++ "\n" ++ content
+    if null content && compareEnoughPar result
+        then putStrLn $ "[" ++ result ++ "]"
+        else getUserInput $ Just result
 
 main :: IO ()
 main = do
-    content <- getContents
-    putStrLn $ "[" ++ content ++ "]"
-    someFunc
+    getUserInput Nothing
