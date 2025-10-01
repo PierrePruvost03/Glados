f30d91d4b2c020509768af970521a3fa939ae2d6
Polo <paul.berlioz@epitech.eu>
2025-10-01 19:08:46 +0200

    chore(main): refactoring input management

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,6 @@
 module Main (main) where
 
 -- import Lib
-import Control.Monad (when, forever)
 import System.IO (isEOF)
 import System.Exit (exitSuccess)
 
@@ -22,26 +21,24 @@ countClosedPar :: String -> Int
 countClosedPar txt = sum (map isClosedPar txt)
 
 compareEnoughPar :: String -> Bool
+compareEnoughPar "" = False
 compareEnoughPar txt = countOpenPar txt <= countClosedPar txt
 
-getUserInput :: Maybe String -> IO ()
-getUserInput Nothing = do
-    done <- isEOF
-    when done $ putStrLn "exit" >> exitSuccess
-    content <- getLine
-    case compareEnoughPar content of
-        True -> putStrLn $ "[" ++ content ++ "]"
-        False -> getUserInput $ Just content
-getUserInput (Just txt) = do
-    done <- isEOF
-    when done $ putStrLn "exit" >> exitSuccess
-    content <- getLine
-    case compareEnoughPar $ txt ++ "\n" ++ content of
-        True -> putStrLn $ "[" ++ txt ++ "\n" ++ content ++ "]"
-        False -> getUserInput $ Just $ txt ++ "\n" ++ content
+useCompare :: String -> Either String String
+useCompare str
+    | compareEnoughPar str = Left $ "[" <> str <> "]"
+    | otherwise = Right str
+
+checkCompare :: Either String String -> IO ()
+checkCompare (Right str) = getUserInput str
+checkCompare (Left str) = putStrLn str >> getUserInput "" -- ici on passe la string au parsing à la place de putStrLn
+
+getUserInput :: String -> IO ()
+getUserInput txt = isEOF >>= \x -> checkEof x
+    where
+        checkEof :: Bool -> IO ()
+        checkEof True = putStrLn "exit" >> exitSuccess
+        checkEof _ = getLine >>= \content -> checkCompare $ useCompare $ txt <> content
 
 main :: IO ()
-main = forever $ do
-    done <- isEOF
-    when done $ putStrLn "exit" >> exitSuccess
-    getUserInput Nothing
+main = getUserInput ""
