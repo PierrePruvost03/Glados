231a9a0492079c84f794f0884372157640ccf45e
sami <sami12022005@yahoo.fr>
2025-10-02 16:30:56 +0200

    feat: fully functionning main that can execute lisp code

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,23 @@
 module Main (main) where
 
 -- import Lib
-import System.IO (isEOF)
+
+import AstParsing (parseAstFromSExpr)
+import DataStruct.SExpr (SExpr)
+import DataStruct.Value (Env)
+import Interpreter.BaseEnv (defaultEnv)
+import Interpreter.EvalAst (evalAst)
+import Parser (Parser (runParser), ParsingError, Rest)
+import SExprParser (parseLisp)
 import System.Exit (exitSuccess)
+import System.IO (isEOF)
 
 checkParEnd :: String -> Int -> Bool
 checkParEnd [] i = i > 0
-checkParEnd ('(':xs) i = checkParEnd xs (i + 1)
-checkParEnd (')':_) 0 = False
-checkParEnd (')':xs) i = checkParEnd xs (i - 1)
-checkParEnd (_:xs) i = checkParEnd xs i
+checkParEnd ('(' : xs) i = checkParEnd xs (i + 1)
+checkParEnd (')' : _) 0 = False
+checkParEnd (')' : xs) i = checkParEnd xs (i - 1)
+checkParEnd (_ : xs) i = checkParEnd xs i
 
 isSkip :: Char -> Bool
 isSkip ' ' = True
@@ -17,21 +25,32 @@ isSkip '\n' = True
 isSkip '\t' = True
 isSkip _ = False
 
+parseToSExpr :: String -> Either ParsingError (SExpr, Rest)
+parseToSExpr str = runParser parseLisp (str, (0, 0))
+
 useCompare :: String -> Either String String
 useCompare str
-    | all isSkip str || checkParEnd str 0 = Right str
-    | otherwise = Left $ "[" <> str <> "]" -- les brackets à enlever, c'est pour debug hehe
+  | all isSkip str || checkParEnd str 0 = Right str
+  | otherwise = Left str
 
-checkCompare :: Either String String -> IO ()
-checkCompare (Right str) = getUserInput str
-checkCompare (Left str) = putStrLn str >> getUserInput "" -- ici on passe la string au parsing à la place de putStrLn
+checkCompare :: Either String String -> Env -> IO ()
+checkCompare (Right str) env = getUserInput str env
+checkCompare (Left str) env = case parseToSExpr str of
+  Right (sexpr, _) ->
+    case parseAstFromSExpr sexpr of
+      Right ast ->
+        case evalAst env ast of
+          Right (val, updatedEnv) -> print val >> getUserInput "" updatedEnv
+          Left err -> putStrLn err >> getUserInput "" env
+      Left err -> print err >> getUserInput "" env
+  Left err -> print err >> getUserInput "" env
 
-getUserInput :: String -> IO ()
-getUserInput txt = isEOF >>= \x -> checkEof x
-    where
-        checkEof :: Bool -> IO ()
-        checkEof True = putStrLn "exit" >> exitSuccess
-        checkEof _ = getLine >>= \content -> checkCompare $ useCompare $ txt <> content
+getUserInput :: String -> Env -> IO ()
+getUserInput txt env = isEOF >>= \x -> checkEof x
+  where
+    checkEof :: Bool -> IO ()
+    checkEof True = putStrLn "exit" >> exitSuccess
+    checkEof _ = getLine >>= \content -> checkCompare (useCompare $ txt <> content) env
 
 main :: IO ()
-main = getUserInput ""
+main = getUserInput "" defaultEnv
