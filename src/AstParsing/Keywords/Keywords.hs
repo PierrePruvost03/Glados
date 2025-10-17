94002aa397253bb2ceecef7077da4e19b862fedb
LixiosDelios <alexandre.guillaud@epitech.eu>
2025-10-17 10:52:24 +0200

    refactor(astParsing): simplify parsing logic and update module exports

--- src/AstParsing/Keywords/Keywords.hs
+++ src/AstParsing/Keywords/Keywords.hs
@@ -1,19 +1,52 @@
 module AstParsing.Keywords.Keywords where
 
+symbolFunc :: String
 symbolFunc = "Funk"
+
+symbolConst :: String
 symbolConst = "Konst"
+
+symbolUnsigned :: String
 symbolUnsigned = "Kong"
+
+symbolLong :: String
 symbolLong = "Strong"
+
+symbolInt :: String
 symbolInt = "Int"
+
+symbolFloat :: String
 symbolFloat = "Float"
+
+symbolBool :: String
 symbolBool = "Bool"
+
+symbolChar :: String
 symbolChar = "Char"
+
+symbolString :: String
 symbolString = "String"
+
+symbolVecIn :: Char
 symbolVecIn = '<'
+
+symbolVecOut :: Char
 symbolVecOut = '>'
+
+symbolArrayIn :: Char
 symbolArrayIn = '['
+
+symbolArrayOut :: Char
 symbolArrayOut = ']'
+
+symbolTuple :: Char
 symbolTuple = '|'
+
+wrapperList :: String
 wrapperList = "<|["
+
+symbolTrue :: String
 symbolTrue = "true"
+
+symbolFalse :: String
 symbolFalse = "false"
--- src/AstParsing/Utils.hs
+++ src/AstParsing/Utils.hs
@@ -1,15 +1,18 @@
-module AstParsing.Utils where
+module AstParsing.Utils
+  ( parseMultiple,
+    parseName,
+  )
+where
 
-import Parser
-import DataStruct.Ast
-import AstParsing.Keywords.Keywords
 import AstParsing.Skip
+import Parser
 
 parseMultiple :: Parser a -> Parser [a]
 parseMultiple p = ((:) <$> p <*> many (skip *> parseChar ',' *> p)) <|> pure []
 
 parseName :: Parser String
-parseName = skip *>
-    parseSomeUntilAnyNotChar
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+parseName =
+  skip
+    *> parseSomeUntilAnyNotChar
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
     <* skip
