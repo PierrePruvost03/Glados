92f4ed84e6533c3a9a826353b4472327121d6e9f
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-10-19 21:31:08 +0200

    feat: New Ast, Bytecode and VM data struct (could change if needed)

---
+++ Kong/src/DataStruct/VM.hs
@@ -0,0 +1,39 @@
+{-# OPTIONS_GHC -Wno-partial-fields #-}
+
+module DataStruct.VM
+  ( VMState(..)
+  , Env
+  , Heap
+  , Stack
+  , HeapAddr
+  , initVMState
+  ) where
+
+import qualified Data.Vector as V
+import qualified Data.Map.Strict as M
+import DataStruct.Bytecode (Value, Instr)
+
+-- Types de base de la VM
+type Env = M.Map String Value
+type HeapAddr = Int
+type Heap = V.Vector Value
+type Stack = [Value]
+
+-- État complet de la VM Kong
+data VMState = VMState
+  { stack :: Stack
+  , env :: Env
+  , heap :: Heap
+  , code :: [Instr]
+  , ip :: Int  -- Instruction Pointer
+  } deriving (Show)
+
+-- Initialisation d'un état VM vide
+initVMState :: [Instr] -> VMState
+initVMState instructions = VMState
+  { stack = []
+  , env = M.empty
+  , heap = V.empty
+  , code = instructions
+  , ip = 0
+  }
