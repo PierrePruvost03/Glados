fdb31317a09600b60546a090710e6f4b6aedd918
abeldaverio <abel.daverio@epitech.eu>
2025-10-24 10:20:01 +0200

    feat(vm): binary file reading

---
+++ Kong/app/ExecuteVM.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module ExecuteVM (executeVM) where
+
+import VM.Execution (exec, makeIntValue)
+import DataStruct.Bytecode.Value (Instr)
+import Data.Binary (decodeFile)
+import DataStruct.VM (VMState(..), baseState)
+import VM.Errors (ExecError(..))
+import Control.Exception
+import System.IO
+-- import GHC.Exception.Type (isDoesNotExistError)
+
+executeVM :: String -> IO Int
+executeVM file = (((decodeFile file) :: IO [Instr]) >>= \instr -> exec (baseState instr) >>= \case
+        (VMState {stack = x : _}) -> pure (makeIntValue x)
+        _ -> throwIO $ InvalidStackAccess
+    ) `catches`  [
+        Handler (\(ex :: ExecError) -> hPutStrLn stderr (show ex) >> pure 1),
+        Handler (\(_ :: ErrorCall) -> hPutStrLn stderr
+            "Failed to decode binary file" >> pure 1),
+        Handler (\(_ :: IOException) -> hPutStrLn stderr (
+            "Binary file " <> file <> " does not exist or cannot be read: ") >> pure 1)
+    ]
