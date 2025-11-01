d62aa76e65f35e49b9a8316b2b3acb461d400bcd
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-11-01 17:06:20 +0100

    refactor(main): move helpers functions to src to keep a Main.hs lite

--- Kong/app/Main.hs
+++ Kong/app/Main.hs
@@ -1,112 +1,39 @@
 module Main (main) where
 
 import System.Environment (getArgs)
-import System.Exit (exitSuccess, exitFailure, exitWith, ExitCode (..) )
+import System.Exit (exitFailure, exitWith, ExitCode(..))
 import System.IO (hPutStrLn, stderr)
-import DataStruct.VM (baseState, VMState (VMState, stack))
-import Compiler.BytecodeGen.Program.Program (compileProgram)
-import Compiler.Include (sortByDependencies, applySelectiveImports, validateIncludes, validateNoDuplicateSymbols, IncludeError(..))
-import Compiler.Type.Error (ProgramError(..), prettyError)
-import VM.Execution (exec)
-import DataStruct.Ast (Ast)
-import DataStruct.Bytecode.Value (Instr(..), Value (VNumber))
-import DataStruct.Bytecode.Number (Number(VInt))
-import AstParsing.BaseParsing (parseAst)
-import AstParsing.ErrorMessage (printParsingResult)
-import Parser (runParser)
-import System.FilePath (dropExtension, takeDirectory, makeRelative)
-import qualified Data.Set as S
+import ExecuteVM (executeVM)
+import Loader (loadAndValidateFiles)
+import ErrorPrinter (printIncludeError)
+import Runtime (compileAndExecute)
 
 main :: IO ()
 main = getArgs >>= handleArgs
 
 handleArgs :: [String] -> IO ()
-handleArgs [] = hPutStrLn stderr "Usage: glados <source-file> [additional-files...]" >> exitFailure
-handleArgs files = loadAndValidateFiles files >>= handleLoad
+handleArgs [] = printUsage >> exitFailure
+handleArgs ("--exec" : file : []) = executeVM file >>= exitWith . toExitCode
+handleArgs files = loadAndValidateFiles files >>= \result ->
+    handleLoad result printIncludeError compileAndExecute
 
-loadAndValidateFiles :: [FilePath] -> IO (Either IncludeError [(String, [Ast])])
-loadAndValidateFiles filePaths =
-  mapM (parseFile baseDir) filePaths >>= processFiles
-  where
-    baseDir = case filePaths of
-      [] -> "."
-      (first:_) -> takeDirectory first
+printUsage :: IO ()
+printUsage = hPutStrLn stderr $ unlines
+    [ "Usage: glados [OPTIONS] <source-file> [additional-files...]"
+    , ""
+    , "Options:"
+    , "  --exec <file>    Execute a pre-compiled bytecode file"
+    , ""
+    , "Examples:"
+    , "  glados main.kong lib.kong         # Compile multiple Kong files"
+    , "  glados --exec program.bytecode    # Execute pre-compiled bytecode"
+    ]
 
-    processFiles parsedFiles = case sequence parsedFiles of
-      Left err -> return $ Left err
-      Right fileAsts -> validateAndSort fileAsts
+toExitCode :: Int -> ExitCode
+toExitCode 0 = ExitSuccess
+toExitCode n = ExitFailure n
 
-    validateAndSort fileAsts = case validateIncludes fileAsts providedFiles of
-      Left err -> return $ Left err
-      Right _ -> case validateNoDuplicateSymbols fileAsts of
-        Left err -> return $ Left err
-        Right _ -> sortAndFilter fileAsts
-
-    sortAndFilter fileAsts = case sortByDependencies fileAsts of
-      Left err -> return $ Left err
-      Right sorted -> return $ applySelectiveImports sorted
-
-    providedFiles = S.fromList $ map (normalizeFilePath baseDir) filePaths
-
-parseFile :: FilePath -> FilePath -> IO (Either IncludeError (String, [Ast]))
-parseFile baseDir filePath =
-  readFile filePath >>= parseContent
-  where
-    fileName = normalizeFilePath baseDir filePath
-
-    parseContent content = case printParsingResult content parseAst of
-      Right str ->
-        return $ Left $ ParseError filePath str
-      Left asts -> return $ Right (fileName, asts)
-
-normalizeFilePath :: FilePath -> FilePath -> String
-normalizeFilePath baseDir path = dropExtension (makeRelative baseDir path)
-
-handleLoad :: Either IncludeError [(String, [Ast])] -> IO ()
-handleLoad (Left err) = printIncludeError err
-handleLoad (Right fileAsts) = handleCompile fileAsts
-
-handleCompile :: [(String, [Ast])] -> IO ()
-handleCompile fileAsts =
-    either printCompileError handleExec $ compileProgram fileAsts
-
-printIncludeError :: IncludeError -> IO ()
-printIncludeError (FileNotFound path) =
-    hPutStrLn stderr ("[Include error] File not found: " ++ path) >> exitFailure
-printIncludeError (CircularDependency file chain) =
-    hPutStrLn stderr ("[Include error] Circular dependency detected in " ++ file ++
-                     ": " ++ show chain) >> exitFailure
-printIncludeError (ParseError _ msg) =
-    hPutStrLn stderr ("[Parsing error] " ++ msg) >> exitFailure
-printIncludeError (MissingInclude file missing) =
-    hPutStrLn stderr ("[Include error] File '" ++ file ++ "' includes '" ++ missing ++
-                     "' but it was not provided in compilation arguments") >> exitFailure
-printIncludeError (MissingSymbol requester included symbol) =
-    hPutStrLn stderr ("[Include error] File '" ++ requester ++ "' requests symbol '" ++ symbol ++
-                     "' from '" ++ included ++ "' but it does not exist") >> exitFailure
-printIncludeError (DuplicateSymbol symbol files) =
-    hPutStrLn stderr ("[Include error] Symbol '" ++ symbol ++ "' is defined in multiple files: " ++
-                     show files) >> exitFailure
-
-printCompileError :: [ProgramError] -> IO ()
-printCompileError errs = do
-    hPutStrLn stderr "[Compilation error]"
-    mapM_ (\(ProgramError file _ err) -> 
-        hPutStrLn stderr $ "  In file '" ++ file ++ "': " ++ prettyError err) errs
-    exitFailure
-
-handleExec :: [Instr] -> IO ()
-handleExec instrs = do
-    putStrLn $ "[Bytecode] " ++ show instrs --debug
-    exec (baseState instrs) >>= printResult
-
-printResult :: VMState -> IO ()
-printResult result@(VMState {stack = (VNumber (VInt r)) : _}) =
-    putStrLn ("[Execution finished] Final VM state: " ++ show result) >> returnValue r
-printResult result = putStrLn ("[Execution finished] Final VM state: " ++ show result ++
-    "\n invalid return type") >> returnValue 1
-
-
-returnValue :: Int -> IO ()
-returnValue 0 = exitSuccess
-returnValue n = exitWith (ExitFailure n)
+-- Load files and handle the result
+handleLoad :: Either a [(String, b)] -> (a -> IO ()) -> ([(String, b)] -> IO ()) -> IO ()
+handleLoad (Left err) onError _ = onError err
+handleLoad (Right fileAsts) _ onSuccess = onSuccess fileAsts
