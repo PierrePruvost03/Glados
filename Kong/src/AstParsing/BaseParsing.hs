e15e6f2a255248d27d8a38a23f5e2a0cc20dbdc9
LixiosDelios <alexandre.guillaud@epitech.eu>
2025-11-02 12:08:41 +0100

    feat(astParsing): extend `parseFor` to support `parseDeclaration` for enhanced loop declaration parsing

--- Kong/src/AstParsing/BaseParsing.hs
+++ Kong/src/AstParsing/BaseParsing.hs
@@ -443,7 +443,7 @@ parseFor =
       *> skip
       *> parseChar symbolForIn
       *> ( ALoop
-             <$> optional (wrap $ AExpress <$> parseExpression) <* skip
+             <$> optional (parseDeclaration <|> wrap (AExpress <$> parseExpression)) <* skip
              <*> ((parseChar symbolForSep *> wrap (AExpress <$> parseExpression)) <|> fatal "For" "invalid condition")
              <*> (parseChar symbolForSep *> optional (wrap (AExpress <$> parseExpression)))
              <*> ((parseChar symbolForOut <|> fatal "For" ("missing char \"" <> [symbolForOut] <> "\"")) *> skip *> (parseBody <|> fatal "For" "invalid body"))
